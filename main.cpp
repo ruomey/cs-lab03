90eb9ecfbdcbd6ef0e11c3843c40a834fb74f518
ErmakovaPA <yermakovapa@mpei.ru>
2022-03-28 13:29:21 +0300

    code: added svg histogram

--- main.cpp
+++ main.cpp
@@ -83,13 +83,26 @@ void svg_end() {
 void svg_text(double left, double baseline, string text) {
     cout << "<text x='" << left << "' y='"<< baseline <<"'>'"<< text <<"'</text>";
 }
-void svg_rect(double x, double y, double width, double height){
-    cout << "<rect x='"<< x <<"' y='"<< y <<"' width='"<< width <<"' height='"<< height <<"'></rect>";
+void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill ="black"){
+    cout << "<rect x='"<< x <<"' y='"<< y <<"' width='"<< width <<"' height='"<< height <<"' stroke='"<< stroke <<"' fill='"<< fill <<"'></rect>";
 
 }
 void show_histogram_svg(const vector<size_t>& bins) {
-    svg_begin(400, 300);
-    svg_rect(50,0,bins[0] * 100,30);
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    double top = 0;
+    for (size_t bin : bins) {
+        const double bin_width = BLOCK_WIDTH * bin;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,"#6a655d","#edb021");
+        top += BIN_HEIGHT;
+    }
     svg_end();
 }
 int main(){
