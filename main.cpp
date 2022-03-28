6d2731ecdee500cda2fdad747e2ffc9546427602
ErmakovaPA <yermakovapa@mpei.ru>
2022-03-28 11:04:20 +0300

    code: decomposed program into function

--- main.cpp
+++ main.cpp
@@ -3,39 +3,29 @@
 #include <math.h>
 using namespace std;
 
-int main(){
-    //Ввод данных
-    size_t number_count;
-    cerr << "Enter number count:";
-    cin >> number_count;
-
-    vector<double> numbers(number_count);
-    cerr << "Enter numbers:";
-    for(size_t i = 0; i < number_count; i++){
-        cin >> numbers[i];
+vector<double> input_numbers(size_t count) {
+    vector<double> result(count);
+    for (size_t i = 0; i < count; i++) {
+        cin >> result[i];
     }
+    return result;
+}
 
-    size_t bin_count;
-    cerr << "Enter bin count:";
-    cin >> bin_count;
-    //Расчет гистограммы
-
-    double min = numbers[0];
-    double max = numbers[0];
-
+void find_minmax(vector<double> numbers, double& min, double& max) {
+    min = numbers[0];
+    max = numbers[0];
     for (double number : numbers){
-        if (min > number){
-            min = number;
-        }
-        if (max < number){
-            max = number;
-        }
+        if (min > number) min = number;
+        if (max < number) max = number;
     }
 
-
-    vector<size_t> bins(bin_count, 0);
+}
+vector<size_t> make_histogram(const vector<double> numbers, size_t bin_count){
+    double min,max;
+    find_minmax(numbers, min, max);
+    vector<size_t>bins(bin_count,0);
     double bin_size = (max - min)/bin_count;
-    for (size_t i = 0; i < number_count; i++){
+    for (size_t i = 0; i < numbers.size(); i++){
         bool found = false;
         for(size_t j = 0; j < (bin_count - 1) && !found; j++){
             auto lo = min +  j * bin_size;
@@ -49,20 +39,17 @@ int main(){
             bins[bin_count - 1]++;
         }
     }
-
-
-    //Вывод гистограммы
+    return bins;
+}
+void show_histogram_text(vector<size_t>bins){
     const size_t SCREEN_WIDTH = 80;
     const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
     size_t max_bin = bins[0];
-
     for (size_t bin : bins){
         if (max_bin < bin){
             max_bin = bin;
         }
     }
-
-
     for (size_t bin: bins){
 
         size_t height = bin;
@@ -78,5 +65,22 @@ int main(){
         }
         cout << endl;
     }
+}
+
+int main(){
+    //Ввод данных
+    size_t number_count;
+    cerr << "Enter number count:";
+    cin >> number_count;
+    cerr << "Enter numbers:";
+    const auto numbers = input_numbers(number_count);
+    size_t bin_count;
+    cerr << "Enter bin count:";
+    cin >> bin_count;
+    //Расчет гистограммы
+    const auto bins = make_histogram(numbers, bin_count);
+
+    //Вывод гистограммы
+    show_histogram_text(bins);
     return 0;
 }
