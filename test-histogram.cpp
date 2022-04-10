dff58bac1d92f983f84ab57fe37009aaf6c023f4
ErmakovaPA <yermakovapa@mpei.ru>
2022-04-10 22:31:07 +0300

    unit tests

---
+++ test-histogram.cpp
@@ -0,0 +1,50 @@
+#include "histogram.h"
+
+#include <cassert>
+
+void test_positive() {
+    double min = 0;
+    double max = 0;
+    find_minmax({1, 2, 3}, min, max);
+    assert(min == 1);
+    assert(max == 3);
+}
+
+void test_negative(){
+    double min = 0;
+    double max = 0;
+    find_minmax({-1, -2, -3}, min, max);
+    assert(min == -3);
+    assert(max == -1);
+
+}
+void equal(){
+    double min = 0;
+    double max = 0;
+    find_minmax({3, 3, 3}, min, max);
+    assert(min == 3);
+    assert(max == min);
+
+}
+void one_num(){
+    double min = 0;
+    double max = 0;
+    find_minmax({1}, min, max);
+    assert(min == 1);
+    assert(max == 1);
+
+}
+void test_void(){
+    double min = 0;
+    double max = 0;
+    find_minmax({}, min, max);
+    assert(min == 0);
+    assert(max == 0);
+
+}
+int main() {
+    test_positive();
+    test_negative();
+    equal();
+}
+
