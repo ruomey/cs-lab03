4dee886f75e36d7fc79c34bd32d5c6ccc18fa801
ErmakovaPA <yermakovapa@mpei.ru>
2022-05-15 23:58:08 +0300

    added work with cURL

--- main.cpp
+++ main.cpp
@@ -32,14 +32,18 @@ Input read_input(istream& in, bool prompt) {
 
 
 
-
 int main(int argc, char* argv[]) {
+    curl_global_init(CURL_GLOBAL_ALL);
     if (argc > 1) {
-        printf("%d %s", argc, *argv);
+        CURL *curl = curl_easy_init();
+        if(curl) {
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+        }
         return 0;
     }
-    //¬вод данных
-    curl_global_init(CURL_GLOBAL_ALL);
     const auto data = read_input(cin,true);
     const auto bins = make_histogram(data);
     show_histogram_svg(bins);
