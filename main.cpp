6fbae1d6b82e3476af7bceaf653c01b2f131b9d2
ErmakovaPA <yermakovapa@mpei.ru>
2022-05-15 19:05:24 +0300

    replaced input with read_input(cin)

--- main.cpp
+++ main.cpp
@@ -5,6 +5,12 @@
 #include "svg.h"
 using namespace std;
 
+
+struct Input {
+    vector<double> numbers;
+    size_t bin_count;
+};
+
 vector<double> input_numbers(istream &in, size_t count) {
     vector<double> result(count);
     for (size_t i = 0; i < count; i++) {
@@ -14,20 +20,30 @@ vector<double> input_numbers(istream &in, size_t count) {
 }
 
 
+Input read_input(istream& in) {
+    Input data;
+
+    cerr << "Enter number count: ";
+    size_t number_count;
+    in >> number_count;
+
+    cerr << "Enter numbers: ";
+    data.numbers = input_numbers(in, number_count);
+
+    cerr << "Enter bin count: ";
+    in >> data.bin_count;
+
+    return data;
+}
+
+
+
+
 int main(){
     //¬вод данных
-    size_t number_count;
-    cerr << "Enter number count:";
-    cin >> number_count;
-    cerr << "Enter numbers:";
-    const auto numbers = input_numbers(cin,number_count);
-    size_t bin_count;
-    cerr << "Enter bin count:";
-    cin >> bin_count;
-    size_t height_historgam;
-    cerr << "Enter height historgram:";
-    cin >> height_historgam;
-    const auto bins = make_histogram(numbers, bin_count);
-    show_histogram_svg(bins, height_historgam);
+    Input data;
+    data = read_input(cin);
+    const auto bins = make_histogram(data.numbers, data.bin_count);
+    show_histogram_svg(bins);
     return 0;
 }
--- svg.h
+++ svg.h
@@ -6,9 +6,8 @@
 using namespace std;
 void svg_begin(double width, double height);
 void svg_end();
-void svg_text(double left, double baseline, string text,double font_size);
+void svg_text(double left, double baseline, string text);
 void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill ="black");
-void show_histogram_svg(const vector<size_t> bins, size_t height_bin);
-void test_height(size_t &height_bin,size_t bins_count, const size_t IMAGE_HEIGHT);
+void show_histogram_svg(const vector<size_t> &bins);
 
 #endif // SVG_H_INCLUDED
