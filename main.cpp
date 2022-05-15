874d29dd12a34852ac931dd4382e7441908f859d
ErmakovaPA <yermakovapa@mpei.ru>
2022-05-16 00:02:49 +0300

    added out of error

--- main.cpp
+++ main.cpp
@@ -40,6 +40,10 @@ int main(int argc, char* argv[]) {
             CURLcode res;
             curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
             res = curl_easy_perform(curl);
+            if (res!= 0) {
+                printf("%s",curl_easy_strerror(res));
+                exit(1);
+            }
             curl_easy_cleanup(curl);
         }
         return 0;
