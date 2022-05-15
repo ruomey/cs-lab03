65ac168e76b84627e4ae28ae05febd393d157c43
ErmakovaPA <yermakovapa@mpei.ru>
2022-05-15 18:01:11 +0300

    added istream &in

--- main.cpp
+++ main.cpp
@@ -5,20 +5,22 @@
 #include "svg.h"
 using namespace std;
 
-vector<double> input_numbers(size_t count) {
+vector<double> input_numbers(istream &in, size_t count) {
     vector<double> result(count);
     for (size_t i = 0; i < count; i++) {
-        cin >> result[i];
+        in >> result[i];
     }
     return result;
 }
+
+
 int main(){
     //¬вод данных
     size_t number_count;
     cerr << "Enter number count:";
     cin >> number_count;
     cerr << "Enter numbers:";
-    const auto numbers = input_numbers(number_count);
+    const auto numbers = input_numbers(cin,number_count);
     size_t bin_count;
     cerr << "Enter bin count:";
     cin >> bin_count;
